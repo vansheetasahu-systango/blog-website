20c0373d4c25723e4fac6ebc31b414eeaefc4ef0
Vansheeta Sahu <vansheeta.sahu@systango.com>
2025-01-14 19:09:04 +0530

    added header

---
+++ app/admin/page.jsx
@@ -0,0 +1,13 @@
+// import CountCard from "./components/CountCard";
+// import { StickyNote, UserRound, List } from 'lucide-react'
+
+export default function Page() {
+    return <main className="p-10">
+        <h1>admin panel</h1>
+        {/* <div className="flex gap-4">
+            <CountCard name={'Posts'} path={'posts'} icon={<StickyNote />} />
+            <CountCard name={'Authors'} path={'authors'} icon={<UserRound />} />
+            <CountCard name={'Categories'} path={'categories'} icon={<List />} />
+        </div> */}
+    </main>
+}
--- app/page.js
+++ app/page.js
@@ -1,15 +1,12 @@
-import Image from "next/image";
+//  import Image from "next/image";
+// import Header from "./components/Header/Header";
+// export const dynamic = 'force-dynamic'
+ 
 
 export default function Home() {
   return (
-   <div className="flex flex-col items-center justify-center h-screen">
-    <h1 className="text-5xl font-bold mb-4">Welcome to my website
-      </h1>
-      <p className="text-lg mb-4">This is a website built with Next.js</p>
-<p className="text-lg mb-4">This is a paragraph of text.</p>
- 
+    <main> 
      
-
-   </div>
+    </main>
   );
 }
