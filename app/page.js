2ed3c1dc5fb543b84a73cca0a7ef5fb563180e94
Vansheeta Sahu <vansheeta.sahu@systango.com>
2025-01-14 11:53:21 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className="flex flex-col items-center justify-center h-screen">
+    <h1 className="text-5xl font-bold mb-4">Welcome to my website
+      </h1>
+      <p className="text-lg mb-4">This is a website built with Next.js</p>
+<p className="text-lg mb-4">This is a paragraph of text.</p>
+ 
+     
+
+   </div>
+  );
+}
